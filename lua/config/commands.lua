a3616b65e051dc952b8f9feaa88d083089455670
Bram Vandenbogaerde <bram@vdb.space>
2026-04-06 21:37:39 +0200

    feat: added command to create non-existing directories

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -2,8 +2,19 @@
 -- Custom commands
 ------------------------------
 
+-- Easy shortcut to formatting
 vim.api.nvim_create_user_command("Format", function()
   vim.lsp.buf.format({ async = true })
 end, {})
 
 vim.cmd("cabbrev format Format")
+
+-- Shortcut to create directories for a file if they
+-- do not exist yet.
+vim.api.nvim_create_user_command("Wd", function()
+  local path = vim.fn.expand("%:p:h")
+  vim.fn.mkdir(path, "p")
+  vim.cmd("write")
+end, {})
+
+vim.cmd("cabbrev wd Wd")
